95a14b12cc24cc342a1877530ba0cf2c12579c06
Leonardo Schuquel <lschuquel.engsoft@gmail.com>
2026-01-06 16:11:56 -0300

    product details creation

--- utils/help.py
+++ utils/help.py
@@ -1,2 +1,7 @@
 def format_price(val):
-    return f'R$ {val:.2f}'.replace('.', ',')
+    try:
+        # Tenta converter para float, se for None ou '' vira 0 ou falha
+        val = float(val) if val else 0
+        return f'R$ {val:.2f}'.replace('.', ',')
+    except (ValueError, TypeError):
+        return f'R$ 0,00'
